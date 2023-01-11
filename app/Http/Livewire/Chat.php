a945de979137794275b7834ffcf942854f52a027
tahir-hash <mbayepro6@gmail.com>
2023-01-11 12:43:03 +0000

    first commit

---
+++ app/Http/Livewire/Chat.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class Chat extends Component
+{
+    public array $chats = [];
+
+    public string $input = '';
+
+    public function render(): View
+    {
+        return view('livewire.chat');
+    }
+
+    public function submit()
+    {
+        $this->chats[] = ['user' => 'human', 'request' => $this->input];
+
+        $result = OpenAI::completions()->create([
+            'max_tokens' => 100,
+            'model' => 'text-davinci-003',
+            'prompt' => $this->input
+        ]);
+        
+        $this->chats[] = [
+            'user' => 'ai',
+            'response' => array_reduce(
+                $result->toArray()['choices'],
+                fn(string $result, array $choice) => $result . $choice['text'],
+                ""
+            )
+        ];
+        $this->input = "";
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mbaye Bot</title>
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+
+<body>
+<main class="h-screen w-screen bg-stone-200 flex justify-center items-center">
+    <div class="container bg-stone-100 gap-4 max-w-4xl h-[700px] rounded-xl p-4 flex justify-start flex-col">
+        <livewire:chat/>
+    </div>
+</main>
+@livewireScripts
+</body>
+
+</html>
